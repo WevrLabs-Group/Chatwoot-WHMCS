4995e4fd9ccb98a9e4d2291f9e9a088a67296fde
WevrLabs <wevrlabs@gmail.com>
2021-05-21 02:39:28 +0200

    Update hooks.php

--- chatwoot/hooks.php
+++ chatwoot/hooks.php
@@ -195,5 +195,10 @@ function hook_chatwoot_logout_footer_output($vars) {
      echo $chatwoot_logoutJS;
 }   
 
+	$whmcsversion = Capsule::table('tblconfiguration')->where('setting','Version')->value('value');
+    $whmcsver = substr($whmcsversion,0,1);
+    if ($whmcsver > 7) {$ClientAreaPageLogout = 'ClientLogout';}
+    else {$ClientAreaPageLogout = 'ClientAreaPageLogout';}
+
 add_hook('ClientAreaHeaderOutput', 1, 'hook_chatwoot_footer_output');
-add_hook('ClientLogout', 1, 'hook_chatwoot_logout_footer_output');
+add_hook($ClientAreaPageLogout, 1, 'hook_chatwoot_logout_footer_output');
