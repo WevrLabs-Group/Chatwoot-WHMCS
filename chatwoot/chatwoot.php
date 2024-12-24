2d27bfb5d125a80c086f11e9b3f2942bdea89888
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2024-12-24 00:22:26 +0000

    Update chatwoot.php

--- chatwoot/chatwoot.php
+++ chatwoot/chatwoot.php
@@ -2,7 +2,7 @@
 
 /***************************************************************************
 // *                                                                       *
-// * Chatwoot WHMCS Addon (v2.0.3).                                        *
+// * Chatwoot WHMCS Addon (v2.0.4).                                        *
 // * This addon module enables you to integrate Chatwoot with your WHMCS    *
 //   and leverage its powerful features.                                   *
 // * Tested on WHMCS Version: v8.12.                                       *
@@ -51,7 +51,7 @@ function chatwoot_config()
     return [
         "name"        => "Chatwoot",
         "description" => "Chatwoot is a customer support tool for instant messaging channels that can help businesses provide exceptional customer support. WHMCS module contributed by: <a href='https://wevrlabs.net/?utm_source=addon_link' target='_blank'>WevrLabs Hosting</a>",
-        "version"     => "2.0.3",
+        "version"     => "2.0.4",
         "author"      => "<a href='https://github.com/WevrLabs-Group/Chatwoot-WHMCS' target='_blank'><img src='https://dash.wevrlabs.net/logo.svg' alt='Contributed by WevrLabs Hosting' width='135px' /></a>",
         "fields"      => [
             'chatwoot_enable'           => [
@@ -61,20 +61,28 @@ function chatwoot_config()
                 'Default'      => 'yes',
                 'Description'  => 'Check to activate the chat box.',
             ],
-            'chatwoot_jscode'           => [
-                'FriendlyName' => 'Website JS Code',
-                'Type'         => 'textarea',
-                'Rows'         => '15',
-                'Cols'         => '100',
+            'chatwoot_url'              => [
+                'FriendlyName' => 'Chatwoot URL',
+                'Type'         => 'text',
+                'Rows'         => '',
+                'Cols'         => '',
                 'Default'      => '',
-                'Description'  => 'Paste your website widget JS code in this field. You can obtain it from your Chatwoot Dashboard > Inboxes > Website > Settings.<br /> For help, visit <a href="https://www.chatwoot.com/docs/product/channels/live-chat/create-website-channel" target="_blank">Chatwoot Docs</a>',
+                'Description'  => 'Enter Chatwoot URL. Example: https://www.chatwoot.com',
+            ],
+            'chatwoot_token'            => [
+                'FriendlyName' => 'Website Widget Token',
+                'Type'         => 'text',
+                'Rows'         => '',
+                'Cols'         => '',
+                'Default'      => '',
+                'Description'  => 'Enter your website widget Token in this field. You can obtain it from your Chatwoot Dashboard > Inboxes > Website > Settings.<br /> For help, visit <a href="https://www.chatwoot.com/hc/user-guide/articles/1677669989-how-to-install-live-chat-on-a-word_press-website" target="_blank">Chatwoot Docs</a>',
             ],
             'chatwoot_verhash'          => [
                 'FriendlyName' => 'Secret Key (Required)',
                 'Type'         => 'text',
                 'Size'         => '',
                 'Default'      => '',
-                'Description'  => 'To make sure the conversations between the customers and the support agents are private and to disallow impersonation, you can setup identity validation in Chatwoot. <br />The key used to generate HMAC hash is unique for each webwidget and you can copy it from Inboxes -> Widget Settings -> Configuration -> Identity Validation -> Copy the token shown there<br />To learn more about this, visit <a href="https://www.chatwoot.com/docs/product/channels/live-chat/sdk/identity-validation" target="_blank">Chatwoot Docs</a>',
+                'Description'  => 'To make sure the conversations between the customers and the support agents are private and to disallow impersonation, you can setup identity validation in Chatwoot. <br />The key used to generate HMAC hash is unique for each webwidget and you can copy it from Inboxes -> Widget Settings -> Configuration -> Identity Validation -> Copy the token shown there<br />To learn more about this, visit <a href="https://www.chatwoot.com/hc/user-guide/articles/1677587479-how-to-enable-identity-validation-in-chatwoot" target="_blank">Chatwoot Docs</a>',
             ],
             'chatwoot_position'         => [
                 'FriendlyName' => 'Chat Box Position',
@@ -88,7 +96,21 @@ function chatwoot_config()
                 'Type'         => 'radio',
                 'Options'      => 'Standard,Expanded Bubble',
                 'Default'      => 'Standard',
-                'Description'  => 'Set the chat box bubble design. Read more at <a href="https://www.chatwoot.com/docs/product/channels/live-chat/sdk/setup#widget-designs" target="_blank">Chatwoot Docs</a>.',
+                'Description'  => 'Set the chat box bubble design. Read more at <a href="https://www.chatwoot.com/hc/user-guide/articles/1677587234-how-to-send-additional-user-information-to-chatwoot-using-sdk" target="_blank">Chatwoot Docs</a>.',
+            ],
+			'chatwoot_launcherTitle'    => [
+                'FriendlyName' => 'Bubble Launcher Title',
+                'Type'         => 'text',
+                'Size'         => '',
+                'Default'      => '',
+                'Description'  => 'Set the chat box bubble design. Read more at <a href="https://www.chatwoot.com/hc/user-guide/articles/1677587234-how-to-send-additional-user-information-to-chatwoot-using-sdk" target="_blank">Chatwoot Docs</a>.',
+            ],
+			'chatwoot_dark'             => [
+                'FriendlyName' => 'Enable Dark Mode on Widget',
+                'Type'         => 'yesno',
+                'Size'         => '55',
+                'Default'      => 'no',
+                'Description'  => 'Check to activate dark mode on the chat box.',
             ],
             'chatwoot_lang'             => [
                 'FriendlyName' => 'Dynamic Language',
